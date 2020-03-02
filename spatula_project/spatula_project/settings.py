a58cdf1e680af3d68f654b4f10f99661d24cf93d
Nathan Welsh <2381793w@student.gla.ac.uk>
2020-03-02 15:14:53 +0000

    added satic and templates

--- spatula_project/spatula_project/settings.py
+++ spatula_project/spatula_project/settings.py
@@ -51,10 +51,15 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'spatula_project.urls'
 
+# Template Dirs
+TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
+
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        # if not spatula/.../templates try spatula_project/.../templates
+        'DIRS': [TEMPLATE_DIR, ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -117,4 +122,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+STATIC_DIR = STATIC_DIR = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [STATIC_DIR, ]
+
+
