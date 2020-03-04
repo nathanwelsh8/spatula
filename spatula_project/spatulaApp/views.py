a0549073a77b7998280ea70355457b5db8bf23a9
Nathan Welsh <2381793w@student.gla.ac.uk>
2020-03-04 13:13:30 +0000

    (very )basic index page

--- spatula_project/spatulaApp/views.py
+++ spatula_project/spatulaApp/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import redirect
 
 # Create your views here.
 # here so the database can be migrated without errors
 def index(request):
-    pass
+    return HttpResponse("Hello World")
