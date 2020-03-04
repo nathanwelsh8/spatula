6e06f2895057770d39ddbfb27912c57b86cba876
Nathan Welsh <2381793w@student.gla.ac.uk>
2020-03-04 11:03:29 +0000

    More models

--- spatula_project/spatulaApp/models.py
+++ spatula_project/spatulaApp/models.py
@@ -1,12 +1,16 @@
 from django.db import models
 from django.template.defaultfilters import slugify
+from django.conf import settings
 from django.contrib.auth.models import User
+import os
 # Create your models here.
+
+NAME_MAX_LENGTH = 128
+MAX_TEXT_LENGTH = 512
 
 class Recipe(models.Model):
 
-    NAME_MAX_LENGTH = 128
-    MAX_TEXT_LENGTH = 512
+    
     DIET_CHOICES = (
                 (1,"Meat"),
                 (2,"Vegetarian"),
@@ -25,8 +29,9 @@ class Recipe(models.Model):
 
     name            = models.CharField(max_length=NAME_MAX_LENGTH)
     ingredients     = models.TextField(max_length=MAX_TEXT_LENGTH)
-    toolsreq        = models.TextField(max_length=MAX_TEXT_LENGTH)
     
+    toolsreq        = models.TextField(max_length=MAX_TEXT_LENGTH)
+    method          = models.TextField()
     # make sure the form views for difficulty, 
     # cost and diettype datatypes restrict the 
     # users selection to the CHOICES above
@@ -36,9 +41,13 @@ class Recipe(models.Model):
    
     #image           = 
     #postedby        = 
+
+    # Following fields are hidden when creating a new recipe
     #ratings are out of 5, to 1 decimal place
-    rating          = models.DecimalField(decimal_places=1,max_digits=3)
+    rating          = models.DecimalField(decimal_places=1,max_digits=3, default=0)
     slug            = models.Slugfield(unique=True)
+    category        = models.ForeignKey(Recipe,to_field="name", on_delete=models.CASCADE)
+
     def __str__(self):
         return self.name
     
@@ -48,12 +57,28 @@ class Recipe(models.Model):
 
 
 class Category(models.Model):
-    pass
+    name = models.CharField(primary_key=True, unique=True, max_length=25)
+
 
 class UserProfile(models.Model):
     pass
 
+
 class Image(models.Model):
-    pass
+
+    def images_path():
+        return os.path.join(settings.IMAGES_DIR, 'images')
+
+    image = models.FilePathField(path=images_path)
+
 class Rating(models.Model):
-    pass
+
+    #auto filled by form so not shown to user 
+    recipe     = models.ForeignKey(Recipe)
+    #user   = models.ForeignKey(Recipe, to_field="poastedBy")
+    
+    # editable by user
+    rating     = models.PositiveSmallIntegerField(decimal_places=2, max_digits=3, default=0)
+    
+    # comments are optional
+    comment    = models.TextField(max_length=MAX_TEXT_LENGTH, required=False)
