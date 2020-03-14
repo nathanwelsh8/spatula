cd5c636039dbc26ceb1d4a4812bb90a39274ebdb
Nathan <36056683+nathanwelsh8@users.noreply.github.com>
2020-03-14 23:18:58 +0000

    manually merging with user_login

--- spatula_project/spatulaApp/forms.py
+++ spatula_project/spatulaApp/forms.py
@@ -46,7 +46,8 @@ class RecipeForm(forms.ModelForm):
 
 
 class UserForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput())
+    username = forms.CharField(max_length=150, widget=forms.TextInput(attrs={'placeholder':'Username'}), label='')
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder':'Password'}), label='')
 
     class Meta:
         model = User
