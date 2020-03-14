5210ec4aaf620a662c2c17cce5c7fd412bca1d15
Nathan Welsh <2381793w@student.gla.ac.uk>
2020-03-14 22:52:23 +0000

    reverse after sucessful registration

--- spatula_project/spatulaApp/views.py
+++ spatula_project/spatulaApp/views.py
@@ -56,6 +56,7 @@ def register(request):
             # Update our variable to indicate that the template
             # registration was successful.
             registered = True
+            return redirect(reverse('spatulaApp:index'))
         else:
             # Invalid form or forms - mistakes or something else?
             # Print problems to the terminal.
