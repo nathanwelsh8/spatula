471409f409602d5049366265665952765dd305aa
Ewan Chelley <2314806c@student.gla.ac.uk>
2020-03-17 16:34:11 +0000

    link to users profile added

--- spatula_project/spatulaApp/urls.py
+++ spatula_project/spatulaApp/urls.py
@@ -3,7 +3,10 @@ from spatulaApp import views
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
- 
+from django.conf.urls import url
+from django.contrib.auth import views as auth_views
+from django.contrib.auth.views import LogoutView
+
  #add url patterns hereg
 app_name = 'spatulaApp'
 
@@ -13,5 +16,6 @@ urlpatterns = [
      path('register/', views.register, name='register'),
      path('add_recipe/', views.add_recipe, name='add_recipe'),
      path('<slug:account_name_slug>/', views.show_profile, name='show_profile'),
-     path('logout/', views.user_logout, name='logout'),
+     path('logout/', views.logout, name='logout'),
+     #path('logout/', LogoutView.as_view(), name='logout'),
  ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
