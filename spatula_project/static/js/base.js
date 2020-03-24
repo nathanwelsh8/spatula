4771abbfda0babed010fe593c021ecdf540e3892
Krzysztof Mrozinski <2395394M@student.gla.ac.uk>
2020-03-24 22:42:18 +0000

    tried to merge stuff w css

--- spatula_project/static/js/base.js
+++ spatula_project/static/js/base.js
@@ -1,52 +1,10 @@
 /* functions to be included in base.html file here */
 
-function sendRequest(text,sort,diet,categories){
-
-    if (sort == undefined){
-
-        $.get(window.location.href,
-        {
-            'search':text,
-        },
-        function(data){
-            console.log(data);
-            $('#recipies').html(data);
-        });
-
-    }else{
-    $.get('/',
-        {
-            'search':text,
-            'sorttype':sort,
-            'diettype':diet,
-            'categories':categories
-        },
-        function(data){
-            console.log(data);
-            $('#recipies').html(data);
-        });
-    }
-        
-}
-
-function getSearchText(){
-    return $('#searchbar').val();
-}
-
-
 function showSignin(){
     var x = document.getElementById("container");
-    if(x.style.display=="none"){
-        x.style.display="block";
+    if(x.style.top == "50px"){
+        x.style.top= "-78px";
     }else{
-        x.style.display="none";
+        x.style.top= "50px";
     }
 }
-
-function openNav() {
-	document.getElementById("mySidenav").style.width = "250px";
-}
-
-function closeNav() {
-	document.getElementById("mySidenav").style.width = "0";
-} 
