263e364eb165dad4b34bb31d29415501439f8dbc
Nathan Welsh <2381793w@student.gla.ac.uk>
2020-03-24 23:02:25 +0000

    IMPORTANT! MUST HAVE THIS VERSION
    Recovered overwritten js files. Search api fixed again

--- spatula_project/static/js/base.js
+++ spatula_project/static/js/base.js
@@ -1,10 +1,53 @@
 /* functions to be included in base.html file here */
 
+
+function sendRequest(text,sort,diet,categories){
+
+    if (sort == undefined){
+
+        $.get(window.location.href,
+        {
+            'search':text,
+        },
+        function(data){
+            console.log(data);
+            $('#recipies').html(data);
+        });
+
+    }else{
+    $.get('/',
+        {
+            'search':text,
+            'sorttype':sort,
+            'diettype':diet,
+            'categories':categories
+        },
+        function(data){
+            console.log(data);
+            $('#recipies').html(data);
+        });
+    }
+        
+}
+
+function getSearchText(){
+    return $('#searchbar').val();
+}
+
+
 function showSignin(){
     var x = document.getElementById("container");
-    if(x.style.top == "50px"){
-        x.style.top= "-78px";
+    if(x.style.display=="none"){
+        x.style.display="block";
     }else{
-        x.style.top= "50px";
+        x.style.display="none";
     }
 }
+
+function openNav() {
+	document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+	document.getElementById("mySidenav").style.width = "0";
+} 
