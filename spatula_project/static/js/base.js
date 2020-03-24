b874a4073646539589957b2d9b186535a8d2c8c3
Lloyd Owen <2387296O@student.gla.ac.uk>
2020-03-24 16:00:32 +0000

    improved responsive css for index page

--- spatula_project/static/js/base.js
+++ spatula_project/static/js/base.js
@@ -8,3 +8,11 @@ function showSignin(){
         x.style.display="none";
     }
 }
+
+function openNav() {
+	document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+	document.getElementById("mySidenav").style.width = "0";
+} 
